21d89dda28c8423fa7315b230e6a70a0a5aef06f
Daniel Freedman <dfreedm@google.com>
2015-01-08 12:56:26 -0800

    Add command line parser, verbose logging
    Note why repos were blacklisted
    Note steps being processed
    Note API call amounts left

--- index.js
+++ index.js
@@ -16,13 +16,72 @@
 var chalk = require('chalk');
 var _ = require('lodash');
 var eyespy = require('./lib/eyespy.js');
+var nopt = require('nopt');
+var path = require('path');
 
-var TOKEN = require('fs').readFileSync('token', 'utf8').trim();
+var options = nopt(
+  {
+    'help': Boolean,
+    'config': path,
+    'token': path,
+    'verbose': Boolean
+  },
+  {
+    '?': ['--help'],
+    'h': ['--help'],
+    'c': ['--config'],
+    't': ['--token'],
+    'v': ['--verbose']
+  }
+);
+
+function printHelp() {
+  var h = chalk.blue;
+  var b = chalk.bold.underline;
+  var d = chalk.dim;
+
+  var help = [
+    b('eyespy') + ': check github org for repositories that need new releases',
+    '',
+    b('Usage:'),
+    '  eyespy ' + d('[OPTIONS]'),
+    '',
+    b('Options:'),
+    '  ' + h('--token') + ', ' + h('-t') + ': Use this file containing a Github OAuth token to authenticate',
+    '  ' + h('--config') + ', ' +  h('-c') + ': JSON config file',
+    '  ' + h('--verbose') + ', ' + h('-v') + ': More verbose output logging operation steps ' + d('(Optional)'),
+    '  ' + h('--help') + ', ' + h('-h') + ', ' + h('-?') + ': Print this message and exit ' + d('(Optional)')
+  ];
+  console.log(help.join('\n'));
+  process.exit(0);
+}
+
+if (options.help || process.argv.length < 3) {
+  printHelp();
+}
+
+if (!options.token) {
+  console.log(chalk.red('Token is required'));
+  process.exit(1);
+}
+
+if (!options.config) {
+  console.log(chalk.red('Config is required'));
+  process.exit(1);
+}
+
+if (options.verbose) {
+  eyespy.setLogger(function(s) {
+    console.log(chalk.dim('LOG: %s'), s);
+  });
+}
+
+var TOKEN = require('fs').readFileSync(options.token, 'utf8').trim();
 
 var LIMIT = 20;
 
 function processConfig() {
-  var config = require('./config.json');
+  var config = require(options.config);
   var bl = config.blacklist;
   if (bl) {
     Object.keys(bl).forEach(function(o) {
@@ -54,15 +113,22 @@ eyespy.getApiLimit(function(err, limits) {
 
 function printOutput(results, limit_start) {
   var actual = _.filter(results, function(r) { return r.commits; });
+  var s;
 
   _.each(actual, function(as) {
-    console.log(chalk.green('%s/%s') + '\t' + chalk.gray('%d'), as.user, as.repo, as.commits);
+    if (options.verbose) {
+      console.log(chalk.green('%s/%s') + '\t' + chalk.dim('%d commits since last release'), as.user, as.repo, as.commits);
+    } else {
+      console.log(chalk.green('%s/%s'), as.user, as.repo);
+    }
   });
-  console.log();
 
-  eyespy.getApiLimit(function(err, limits) {
-    var r = limits.remaining;
-    console.log(chalk.blue('remaining API calls: %d'), r);
-    console.log(chalk.blue('used: %d'), limit_start - r);
-  });
+  if (options.verbose) {
+    console.log();
+    eyespy.getApiLimit(function(err, limits) {
+      var r = limits.remaining;
+      console.log(chalk.blue('remaining API calls: %d'), r);
+      console.log(chalk.blue('used: %d'), limit_start - r);
+    });
+  }
 }
